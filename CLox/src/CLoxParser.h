790560087504db369d18025b949f2fe6173b7eea
MartinSantesteban <martin.p.santesteban@gmail.com>
2024-07-08 15:06:47 -0300

    minor changes

--- CLox/src/CLoxParser.h
+++ CLox/src/CLoxParser.h
@@ -1,10 +1,5 @@
 #include "AST.cpp"
 
-// Each grammar rule becomes a method 
-// El parser agarra los tokens y hace el AST! 
-// Como las expresiones tienen estructura de arbol, vas devolviendo expresiones
-// a medida que avanzas.
-
 class Parser{
     public:
         vector<Token> tokens;
@@ -22,10 +17,6 @@ class Parser{
         Expr term();
         Expr factor();
         Expr unary();
-        */
         Expr primary();
-
-
-        bool matchTokenTypes(vector<tokenType> tts);
-        void advance();
+        */
 };
